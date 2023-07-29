22d70088df202b0273031aa9ec50ef78b42e447f
Didem Cakir <didemydn@gmail.com>
2023-07-29 15:13:45 +0200

    foodbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from "./foods.json";
+import { useState } from 'react';
+import { Row, Divider, Button } from 'antd'
+import FoodBox from './components/FoodBox';
 
 function App() {
+  const [allFood, setAllFood] = useState(foods)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divider>Food List</Divider>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {allFood.map((food)=>(
+          <FoodBox key={food.name} food={food}/>
+        ))}
+      </Row>
     </div>
   );
 }
